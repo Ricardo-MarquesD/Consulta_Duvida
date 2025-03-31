264e69d8328518be710dde108199595073359657
RICARDO <ricardomarquesddsa@gmail.com>
2025-03-31 18:49:31 -0300

    Inicio de java

--- Java/src/App.java
+++ Java/src/App.java
@@ -1,5 +1,11 @@
-public class App {
-    public static void main(String[] args) {
+// Aqui a gente inicializa nosso código com uma Classe, que é o bloco de código principal do arquivo. Formato: tipo_da_classe class nome_do_arquivo
+public class App { // public define que nossa classe pode ser acessada por qualquer outro arquivo java no programa, class define que isto é uma classe, e o nome do arquivo.java
+
+    // Declaramos um método main, que é onde o arquivo será executado primariamente, o static diz que é um método único e travado a classe de origem.
+    public static void main(String[] args) { // o void diz que não retorna nada, e o String[] args, é um parâmetro que pode receber uma lista de argumentos.
+        
+    
+
         
     }
 }
