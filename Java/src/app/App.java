5b8ffe8fdc605772cf0bb12f3c553b026e262a5b
RICARDO <ricardomarquesddsa@gmail.com>
2025-06-04 18:55:28 -0300

    ArrayList

--- Java/src/app/App.java
+++ Java/src/app/App.java
@@ -2,6 +2,7 @@ package app;
 
 import poo.*; // Usando import + nome da passa, conseguimos chamar as classes de outras pastar para esse arquivo
 import java.util.Scanner;
+import java.util.ArrayList;
 
 // Aqui a gente inicializa nosso código com uma Classe, que é o bloco de código principal do arquivo. Formato: tipo_da_classe class nome_do_arquivo
 public class App { // public define que nossa classe pode ser acessada por qualquer outro arquivo java no programa, class define que isto é uma classe, e o nome do arquivo.java
@@ -22,9 +23,17 @@ public class App { // public define que nossa classe pode ser acessada por qualq
     }
 
     public static void Poo(){
+        // Criação comum de entidades
         SubClass sub1 = new SubClass(3, null, "Hi guys", "Hola amigo"); // Dessa forma a gente cria um objeto chamando sua respectiva classe/classe construtora
-        sub1.menssage(); // Chamando os métodos do objetos, que criamos anteriormente
-        sub1.AbsMetod();
+        //sub1.menssage(); // Chamando os métodos do objetos, que criamos anteriormente
+        //sub1.AbsMetod();
+
+        // ArrayList
+        ArrayList<String> lista = new ArrayList<>(); // Criando uma arraylist do tipo string
+        lista.add("Hi bro"); // Adicionando elementos a essa ArrayList
+        lista.add("your is incorrect, I am the true");
+        System.out.println(lista); // Printando toda a arraylist
+        System.out.println(lista.get(0)); // Pritando uma parte especifica
 
     }
 }
--- Java/src/poo/Super.java
+++ Java/src/poo/Super.java
@@ -5,7 +5,7 @@ abstract class Super{ // Criando uma classe abstrata, onde será declarado dados
     private String word2; // Somente pode ser acessada via um intermediario
     protected String word3; // Pode ser acessado de qualquer parte de seu escopo(Super e suas dependentes)
     
-    public Super(String word2){ // Método Construtor da Classe Abstrata
+    protected Super(String word2){ // Método Construtor da Classe Abstrata
         this.word2 = word2;
         if(word2 != (String)word2){ // If para dar inicio a um tratamento de throw
             throw new IllegalArgumentException("Houve um erro na word2!"); // Declarando nosso tipo de erro no tratamento
